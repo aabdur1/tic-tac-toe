4c34e2de7863d93a7e214442430bf5649e73fd08
Amir Abdur-Rahim <amirabdurrahim@gmail.com>
2024-03-03 02:11:48 -0600

    Created board object and renamed squares

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ squares.forEach(function (square) {
   square.addEventListener("click", move);
 });
 
+class Player {
+  constructor(symbol) {
+    this.symbol = symbol;
+    this.isTurn = true;
+  }
+}
+
+const player1 = new Player("X");
+
 function move() {
-  console.log(this.textContent);
+  this.textContent = "X";
+  boardObject[this.classList.value] = player1.symbol;
+  console.log(boardObject);
 }
 
 const board = [
@@ -14,8 +25,17 @@ const board = [
   ["", "", ""],
 ];
 
+const boardObject = {};
+
+board.forEach((row, rowIndex) => {
+  row.forEach((cell, colIndex) => {
+    const key = `row${rowIndex + 1}Col${colIndex + 1}`;
+    boardObject[key] = cell;
+  });
+});
+
 for (let i = 0; i < 3; i++) {
   for (let j = 0; j < 3; j++) {
-    board[i][j] = "X";
+    board[i][j] = `${i} ${j}`;
   }
 }
